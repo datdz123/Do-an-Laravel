ac2b771cd6e2d55158e18b0d2c9f70dd805f8ca3
Dang Quang <quangld@dbiz.vn>
2023-11-30 09:41:44 +0700

    the last commit

--- resources/views/back/layouts/masterlayout.blade.php
+++ resources/views/back/layouts/masterlayout.blade.php
@@ -91,7 +91,7 @@
                 <div class="navbar-custom-menu">
                     <ul class="nav navbar-nav">
                         <!-- Messages: style can be found in dropdown.less-->
-                        {{-- <li class="dropdown messages-menu">
+                        <li class="dropdown messages-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <i class="fa fa-envelope-o"></i>
                                 <span class="label label-success">4</span>
@@ -172,9 +172,9 @@
                                 </li>
                                 <li class="footer"><a href="#">See All Messages</a></li>
                             </ul>
-                        </li> --}}
+                        </li>
                         <!-- Notifications: style can be found in dropdown.less -->
-                        {{-- <li class="dropdown notifications-menu">
+                        <li class="dropdown notifications-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <i class="fa fa-bell-o"></i>
                                 <span class="label label-warning">10</span>
@@ -215,9 +215,9 @@
                                 </li>
                                 <li class="footer"><a href="#">View all</a></li>
                             </ul>
-                        </li> --}}
+                        </li>
                         <!-- Tasks: style can be found in dropdown.less -->
-                        {{-- <li class="dropdown tasks-menu">
+                        <li class="dropdown tasks-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <i class="fa fa-flag-o"></i>
                                 <span class="label label-danger">9</span>
@@ -301,7 +301,7 @@
                                     <a href="#">View all tasks</a>
                                 </li>
                             </ul>
-                        </li> --}}
+                        </li>
                         <!-- User Account: style can be found in dropdown.less -->
                         <li class="dropdown user user-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
