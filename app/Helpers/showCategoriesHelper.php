224de240a63e683d00bf10765dda6f91c69704bd
Dang Quang <quangld@dbiz.vn>
2023-11-30 10:38:26 +0700

    optimal hàm đệ quy hiện thị danh mục sản phẩm

--- app/Helpers/showCategoriesHelper.php
+++ app/Helpers/showCategoriesHelper.php
@@ -2,6 +2,8 @@
 
 namespace App\Helpers;
 
+use App\Models\Product;
+
 class showCategoriesHelper
 {
 
@@ -48,4 +50,77 @@ class showCategoriesHelper
             }
         }
     }
+
+    public static function showCategories($categories, $parent_id = 0, $char = '')
+    {
+        foreach ($categories as $key => $item) {
+            // Nếu là chuyên mục con thì hiển thị
+
+            //count
+            $countProducts = Product::join('product_categories', 'products.product_category_id', '=', 'product_categories.id')
+                ->select('products.*', 'product_categories.parent_id')
+                ->where('product_categories.parent_id', $item->id)
+                ->where('status', 'active')
+                ->count();
+
+            $countSubcategory =  Product::join('product_categories', 'products.product_category_id', '=', 'product_categories.id')
+                ->select('products.*', 'product_categories.parent_id')
+                ->where('product_categories.id', $item->id)
+                ->where('status', 'active')
+                ->count();
+
+            $checked = request('id') == $item->id ? 'checked' : '';
+            if ($item['parent_id'] == $parent_id) {
+                // Xử lý hiển thị chuyên mục
+                if ($item->parent_id == 0) {
+                    echo ' <form method="GET" action="' .
+                        route('shop/category', ['id' => $item->id, 'slug' => $item->slug]) .
+                        '">
+                        <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                            <input ' .
+                        $checked .
+                        '   onclick="this.form.submit()"  type="checkbox" class="custom-control-input" value="' .
+                        $item->id .
+                        '"  id="idd' .
+                        $key .
+                        '">
+                    <label class="custom-control-label"  for="idd' .
+                        $key .
+                        '"> ' .
+                        $item->name .
+                        '</label>
+                    <span class="badge border font-weight-normal">' .
+                        $countProducts .
+                        '</span></div></form>';
+                } else {
+                    echo '<form method="GET" action="' .
+                        route('shop/category', ['id' => $item->id, 'slug' => $item->slug]) .
+                        '">
+                         <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                         <input ' .
+                        $checked .
+                        ' onclick="this.form.submit()" type="checkbox" class="custom-control-input" value="' .
+                        $item->id .
+                        '"  id="id' .
+                        $key .
+                        '">
+                    <label class="custom-control-label" for="id' .
+                        $key .
+                        '">' .
+                        $char .
+                        $item->name .
+                        '</label>
+                    <span class="badge border font-weight-normal">' .
+                        $countSubcategory .
+                        '</span></div></form>';
+                }
+                // Xóa chuyên mục đã lặp
+                unset($categories[$key]);
+                // Tiếp tục đệ quy để tìm chuyên mục con của chuyên mục đang lặp
+                self::showCategories($categories, $item['id'], $char . '|--- ');
+            }
+        }
+    }
+
+
 }
